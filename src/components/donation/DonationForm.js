5aceef6bce44188f4e18ad3d2f006ddf9af62fc5
harperzhu <harperzhu@yahoo.com>
2021-11-24 00:52:31 -0800

    changes

--- src/components/donation/DonationForm.js
+++ src/components/donation/DonationForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Profile from "./Profile"
 
 var siteWidth = 1280;
 // TODO: YOU DONT HAVE SCREEN DEFINED
@@ -7,75 +7,91 @@ var siteWidth = 1280;
 var scale = 0;
 
 document.querySelector('meta[name="viewport"]')
-        .setAttribute(
-            'content',
-            'width=' + siteWidth + ', initial-scale=' + scale
-        );
+  .setAttribute(
+    'content',
+    'width=' + siteWidth + ', initial-scale=' + scale
+  );
 
 export function DonationForm(props) {
 
-    const donationAmountArray = ["$1", "$2", "$3", "$4", "$5"];
-    let createButton = donationAmountArray.map((amount) => 
-        <DonationAmount amount={amount}/>
-    );
+  const donationAmountArray = ["$1", "$2", "$3", "$4", "$5"];
+  let createButton = donationAmountArray.map((amount) =>
+    <DonationAmount amount={amount} />
+  );
 
 
-    return(
-      <div className="donation-window">
-        <h1> You can help save animals today.
-          <br/>
-          For every dollar you donated, 
-          <br/>
-          the selected pet gets one meal.
-          </h1>
+  return (
+    <div className="donation-window">
 
-        <div className="donation-steps">
-            <a href="#" className="donation-step-button donation-step-button-green">Amount</a>
-            <hr className="donation-button-line"></hr>
-            <a href="#" className="donation-step-button donation-step-button-grey">Billing</a>
-            <a href="#" className="donation-step-button donation-step-button-grey">Payment</a>
-        </div>
-        <div className="mobile-container">
-            <div className="payment-amount">
-                {createButton}
-              <CustomDonationAmount/>
-            </div>
-        </div>
-      <DonationABotton/>
+      <DonationSlogan />
+      <DonationSteps />
+      <div className="payment-amount">
+        {createButton}
+        <CustomDonationAmount />
       </div>
-    );
-  
+      <DonationABotton />
+    </div>
+  );
+
 }
 
 
 export function DonationAmount(props) {
-    return (
-        <div className="form-item form-type-radio">
-            <input type="button" id="edit-submitted-donation-amount-2" name="submitted[donation][amount]" value={props.amount}/>
-        </div>
-    );
-  }
+  return (
+    <div className="form-item form-type-radio">
+      <input type="button" id="edit-submitted-donation-amount-2" name="submitted[donation][amount]" value={props.amount} />
+    </div>
+  );
+}
 
 
 
 export function DonationABotton() {
-    return (
-      <div className="progress-buttons">
+  return (
+    <div className="progress-buttons">
       <a href="updates.html" className="donate"> Donate </a>
-  </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 
 
-  export function CustomDonationAmount() {
-    return (
-      <div className="form-item form-type-radio">
-      <input type="number" id="edit-submitted-donation-amount-7" name="submitted[donation][amount]" value="$"/>
+export function CustomDonationAmount() {
+  return (
+    <div className="form-item form-type-radio">
+      <input type="number" id="edit-submitted-donation-amount-7" name="submitted[donation][amount]" value="$" />
       <label for="amount-7"></label>
-  </div>
-    );
-  }
+    </div>
+  );
+}
+
+export function DonationSteps() {
+  return (
+    <div className="donation-steps">
+      <a href="#" className="donation-step-button donation-step-button-green">Amount</a>
+      <hr className="donation-button-line"></hr>
+      <a href="#" className="donation-step-button donation-step-button-grey">Billing</a>
+      <a href="#" className="donation-step-button donation-step-button-grey">Payment</a>
+    </div>
+
+
+  );
+}
+
+export function DonationSlogan() {
+  return (
+    <div>
+      <h1> You can help save animals today.
+        <br />
+        For every dollar you donated,
+        <br />
+        the selected pet gets one meal.
+      </h1>
+    </div>
+
+  );
+}
+
 
 
--- src/components/donation/Profile.js
+++ src/components/donation/Profile.js
@@ -1,46 +1,64 @@
-import React, { Component } from "react";
+import React from "react";
 
 export function Profile(props) {
-    return(
-        <div className="donation-page">
+  return (
+    <div className="donation-page">
 
-            <div className="individual-profile">
-                <div className="profile-background-wrapper">
-                    <div className="picture">
-                        <img src="img/16821635369022_.pic.jpg" alt="dog image" width="460" height="372" />
-                    </div>
+      <div className="individual-profile">
+        <div className="profile-background-wrapper">
+      <CardPicture/>
 
-                    <div className="mobile-container">
-                        <div className="pet-info">
-                            <h2 className="card-title">Pochi</h2>
-                            <div className="card-text">
-                                {/* TODO: REMOVE ALL THE BOLD TAGS AND REPLACE WITH CSS */}
-                                <div className="name">
-                                    <b>Name: Pochi</b>
-                                </div>
-                                <div className="Age">
-                                    <b>Age: 3 years old</b>
-                                </div>
-                                <div className="Health">
-                                    <b>Heath: excellent</b>
-                                </div>
-                                <div className="Size">
-                                    <b>Size: small</b>
-                                </div>
-                                <div className="gender">
-                                    <b>gender: Male</b>
-                                </div>
-                                <div className="X meals left">
-                                    <b>Number of meals left: 1 Meal</b>
-                                </div>
-                            </div>
-                            <div className="donation-button-wrapper">
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div className="mobile-container">
+            <div className="pet-info">
+              <CardTitle />
+              <CardText />
             </div>
+          </div>
         </div>
-        );
+      </div>
+    </div>
+  );
 
+}
+
+export function CardText(props) {
+  let currentPet = props.pets;
+  return (
+    <div className="card-text">
+      {/* TODO: REMOVE ALL THE BOLD TAGS AND REPLACE WITH CSS */}
+      <div className="name">
+        <b>Name: Pochi</b>
+      </div>
+      <div className="Age">
+        <b>Age: 3 years old</b>
+      </div>
+      <div className="Health">
+        <b>Heath: excellent</b>
+      </div>
+      <div className="Size">
+        <b>Size: small</b>
+      </div>
+      <div className="gender">
+        <b>gender: Male</b>
+      </div>
+      <div className="X meals left">
+        <b>Number of meals left: 1 Meal</b>
+      </div>
+    </div>
+  );
+}
+
+export function CardTitle(props) {
+  return (
+    <h2 className="card-title">Pochi</h2>
+  );
+}
+
+
+export function CardPicture(props) {
+  return (
+    <div className="picture">
+      <img src="img/16821635369022_.pic.jpg" alt="dog image" width="460" height="372" />
+    </div>
+  );
 }
