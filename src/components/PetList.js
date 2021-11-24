7242852242c51810e28c2e98313e984e77a9d2f8
Ashley Zhao <ashleyzhao8@gmail.com>
2021-11-23 22:49:52 -0800

    fixing export function

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
 
-export class PetCard extends Component {
-  render() {
+function PetCard(props) {
     return(
       <div className="pet-card">
-        <img className="pet-image" src={this.props.pet.img} alt={this.props.pet.name} />
+        <img className="pet-image" src={props.pet.img} alt={props.pet.name} />
         <div className="pet-content">
-          <h3 className="pet-name">{this.props.pet.name}</h3>
-          <p className="meals-left">{this.props.pet.meals} meals left</p>
+          <h3 className="pet-name">{props.pet.name}</h3>
+          <p className="meals-left">{props.pet.meals} meals left</p>
           <button className="feed-me">Feed Me</button>
         </div>
       </div>
     );
-  }
 }
 
-export class PetList extends Component {
-  render() {
+function PetList(props) {
     return(
         <div id="petList" className="col-9">
           <div className='profile-cards'>
-            {this.props.pets.map((pet) => {
+            {props.pets.map((pet) => {
               return <PetCard key={pet.name} pet={pet} />
             })}
           </div>
         </div>
     );
-  }
 }
 
-export class MyPetToggle extends Component {
-  render() {
+function MyPetToggle(props) {
     return(
       <div> placeholder </div>
     )
-  }
 }
+
+export {PetCard, PetList, MyPetToggle};
