6255df00f9a9528fe3aa83d20fba570d53b45db1
harperzhu <harperzhu@yahoo.com>
2021-12-11 02:21:40 -0800

    add in redirect

--- src/components/shelterAdd/ConfirmPetAddition.js
+++ src/components/shelterAdd/ConfirmPetAddition.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export function ConfirmPetAddition() {
   return(
     <div className="mt-5 pt-4 text-center h-100">
-    <img src="/img/success.png"></img>
+    <img src="/img/success.png" alt="cute pet image indicates successful request"></img>
     <h2>Success! </h2>
-    <h2> We Received Your Request Of Adding A New Pet</h2>
+    <h2> We Received Your Request</h2>
     <br></br>
     <h6>You can view your new added pet by clicking the button below 
     </h6>
