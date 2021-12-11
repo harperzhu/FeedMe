09d893f88ddbfd0a0e209445793881b5d741e26b
harperzhu <harperzhu@yahoo.com>
2021-12-11 00:27:34 -0800

    save data from user

--- src/components/shelterAdd/AddNewPet.js
+++ src/components/shelterAdd/AddNewPet.js
@@ -6,6 +6,36 @@ export function AddNewPet(props) {
 
     let [CurrentAddedPetKind, setCurrentAddedPetKind] = useState(null);
 
+    // let [currentData, setCurrentData] = useState({
+    //     "name" : "",
+    //     "age" : 0,
+    //     "gender" : "",
+    //     "kind" : "",
+    //     "breed" : "",
+    //     "health" : ""
+    // });
+
+    let [currentData, setCurrentData] = useState({
+    });
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        
+        // for (let i = 0; i < 6; i++) {
+        //     console.log(event.target[i].id + " : " + event.target[i].value);
+        // }
+        let data = {};
+        data[event.target[0].value] = {};
+        
+        // DO NOT CHANGE THIS TO A FOREACH OR MAP
+        // THE KEYS ARE STRING NUMBERS
+        for (let i = 1; i < 6; i++) {
+            data[event.target[0].value][event.target[i].id] = event.target[i].value;
+        }
+        setCurrentData(data)
+        console.log(currentData);
+    }
+
 
     const handleCurrentAddedPetKind = (kind) => {
         setCurrentAddedPetKind(kind);
@@ -37,17 +67,21 @@ export function AddNewPet(props) {
             <main id="about-main">
 
                 <div class="form-group my-5 mx-5 px-5">
-                    <form>
+                    <form onSubmit={handleFormSubmit}>
                         <div >
                             <label for="exampleFormControlInput1">Pet Name</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Example: Coffee" onChange={handleFormNameChange}/>
+                            <input type="string"
+                                    class="form-control"
+                                    id="name"
+                                    placeholder="Example: Coffee"
+                            />
                         </div>
 
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Pet Age</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                            <option selected="selected">Select an option</option>
-                                <option> Young (younger than 2 years old)</option>
+                            <select class="form-control" id="age">
+                                <option selected="selected">Select an option</option>
+                                <option value="young"> Young (younger than 2 years old)</option>
                                 <option> Adult ((younger than 5 years old) </option>
                                 <option> Senior (older than 5 years old) </option>
                             </select>
@@ -55,7 +89,7 @@ export function AddNewPet(props) {
 
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Pet Gender</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
+                            <select class="form-control" id="gender">
                             <option selected="selected">Select an option</option>
                                 <option >Female</option>
                                 <option>Male</option>
@@ -91,29 +125,12 @@ export function AddNewPet(props) {
 
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Pet Health</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
+                            <select class="form-control" id="health">
                             <option selected="selected">Select an option</option>
                                 <option>Special Needs</option>
                                 <option>Healthy</option>
                             </select>
                         </div>
-
-
-
-
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Description</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="
-
-                            Example:
-                            
-                            Coffee is a sweet girl and theh perfect companion for a family with adult children. 
-                            She will bond incredibly strong with her ‘special person’. 
-                            She needs an understanding owner who can work with her to become more independent and who has time to be home with her for part of the day.
-                            
-                            "></textarea>
-                        </div>
-
 
                         <br />
                         <form>
@@ -123,13 +140,16 @@ export function AddNewPet(props) {
                                 <input type="file" class="form-control-file" id="exampleFormControlFile1" />
                             </div>
                         </form>
+
+                        <input type="submit" value="submit" />
+
                     </form>
 
                 </div>
 
-                <Link to="/petList" className="btn btn-lg text-uppercase">
-                <button onClick={handleFormSubmit}>Submit</button>
-                </Link>
+                {/* <Link to="/petList" className="btn btn-lg text-uppercase">
+                    <button onSubmit={handleFormSubmit}>Submit</button>
+                </Link> */}
 
             </main>
 
@@ -139,37 +159,35 @@ export function AddNewPet(props) {
 }
 
 
-const [queryInputName, setQueryInputName] = useState('');
+// const [queryInputName, setQueryInputName] = useState('');
 
 
-const handleFormSubmit = (event) => {
-    event.preventDefault();
-  }
 
-  const handleFormNameChange = (event) => {
-    setQueryInputName(event.target.value);
-  }
 
-  const db = getDatabase();
+//   const handleFormNameChange = (event) => {
+//     setQueryInputName(event.target.value);
+//   }
 
-  const UpdateDatabase = () => {
+//   const db = getDatabase();
 
-    const newPetRef = ref(db, "message"); 
-    firebaseSet(newPetRef)
+//   const UpdateDatabase = () => {
+
+//     const newPetRef = ref(db, "message"); 
+//     firebaseSet(newPetRef)
 
 
     
-    const newMessageObj = {
-      userId: msgUser.uid,
-      userName: msgUser.username,
-      userImg: "/img/"+msgUser.userName+".png", //hacky
-      text: msgText,
-      timestamp: Date.now(), //posted now
-      channel: msgChannel
-    }
-    const newMessageArray = [...messageArray, newMessageObj]; //spread to copy!
-    setMessageArray(newMessageArray);
-  }
+//     const newMessageObj = {
+//       userId: msgUser.uid,
+//       userName: msgUser.username,
+//       userImg: "/img/"+msgUser.userName+".png", //hacky
+//       text: msgText,
+//       timestamp: Date.now(), //posted now
+//       channel: msgChannel
+//     }
+//     const newMessageArray = [...messageArray, newMessageObj]; //spread to copy!
+//     setMessageArray(newMessageArray);
+//   }
 
 
     export function PetBreeds(props) {
@@ -219,9 +237,9 @@ function AddNewPetCover() {
 
 
 
-// function PetKind(props) {
+// // function PetKind(props) {
 
 
-//     return (
-//     )
-// }
+// //     return (
+// //     )
+// // }
