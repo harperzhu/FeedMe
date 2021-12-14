620512d792876309091b3e1d1a023b6bf6d3f379
aurorayin <zyin5@uw.edu>
2021-12-13 23:50:16 -0800

    add new pet fixed

--- src/components/shelterAdd/AddNewPet.js
+++ src/components/shelterAdd/AddNewPet.js
@@ -13,20 +13,18 @@ export function AddNewPet(props) {
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
+        
         let name = event.target[0].value;
         let imageFile = event.target[6].files[0];
-
-        //get access to our firebase storage where we store our images
         const storage = getStorage();
-        //make reference to our profile pics in the storage
-        let sRef = storageRef(storage, "/img/profile_pics/" + name + ".png");
-        //take in the image file and push it to firebase reference
+        let sRef = storageRef(storage,  "/img/profile_pics/" + name + ".png");
+
         await uploadBytes(sRef, imageFile);
         let imgUrl = await getDownloadURL(sRef);
-        //initiate the input data object
+
         let data = {};
         data[name] = {};
-
+        
         // lookup a list of all the names
         let invalidNameList = Object.keys(props.pets).map((name) => name.toLowerCase());
 
@@ -37,38 +35,29 @@ export function AddNewPet(props) {
             for (let i = 1; i < 6; i++) {
                 data[name][event.target[i].id] = event.target[i].value;
             }
-            //the key of data is name, but we also store a name inside of the data object
-            //in case we need to reference it
             data[name]["name"] = name;
-            //initiate structure of the data object
             data[name]["img_path"] = "/img/profile_pics/" + name + ".png";
             data[name]["img_url"] = imgUrl;
             data[name]['likes'] = 0;
             data[name]["updates"] = [""];
 
-
-            //set the current data object as the one user just input
+            
             await setCurrentData(data);
-            //upload the new pets from the database
             await uploadToDatabase(data);
-            //reload the petlist page to add in new uploaded pets
             await props.reloadPet();
-            //once we sucessfully received user request, redirect the page to inform the user of the sucessful request
             setShouldRedirect(true);
         } else {
-            //prompt the users if they input a name that is the same as one of the existing pet in database/petlist
+            //error message
             alert("Invalid Name for Pet: Please Rename The Pet");
         }
         return data;
     }
 
 
-    //upload the current data object to database
-    function uploadToDatabase(data) {
+    function uploadToDatabase(data){
         const db = getDatabase();
         const name = Object.keys(data)[0];
-        const petRef = ref(db, "pets/" + name);
-        //set the current pet object with the current data object
+        const petRef = ref(db, "pets/"+name);
         firebaseSet(petRef, data[name]);
     }
 
@@ -76,124 +65,122 @@ export function AddNewPet(props) {
     return (
         <div>
             {!shouldRedirect ?
-            /*if the page shouldn't be redirect, render the input form*/
-                <div>
-                    <AddNewPetCover />
-                    <main>
-                        <div className="form-group my-5 mx-5 px-5">
-                            <form onSubmit={handleFormSubmit}>
-                                <div >
-                                    <label htmlFor="name">Pet Name</label>
-                                    <input type="string"
-                                        className="form-control"
-                                        id="name"
-                                        placeholder="Example: Coffee"
-                                        required="required"
-                                    />
-                                </div>
+            <div>
+            <AddNewPetCover />
+            <main>
 
-                                <div className="form-group">
-                                    <label htmlFor="age">Pet Age  (in years old)</label>
-                                    <select className="form-control" id="age" required>
-                                        <option value="selected">Select an option</option>
-                                        <option> under 1</option>
-                                        <option> 1</option>
-                                        <option> 2</option>
-                                        <option> 3</option>
-                                        <option> 4</option>
-                                        <option> 5</option>
-                                        <option> 6</option>
-                                        <option> 7</option>
-                                        <option> 8</option>
-                                        <option> 9</option>
-                                        <option> 10</option>
-                                        <option> 11</option>
-                                        <option> 12</option>
-                                        <option> 13</option>
-                                        <option> 14</option>
-                                        <option> 15</option>
-                                        <option> 16</option>
-                                        <option> 17</option>
-                                        <option> 18</option>
-                                        <option> 19</option>
-                                        <option> 20</option>
-                                        <option> 20 years old and up</option>
-                                    </select>
-                                </div>
+                <div className="form-group my-5 mx-5 px-5">
+                    <form onSubmit={handleFormSubmit}>
+                        <div >
+                            <label htmlFor="name">Pet Name</label>
+                            <input type="string"
+                                    className="form-control"
+                                    id="name"
+                                    placeholder="Example: Coffee"
+                                    required="required"
+                            />
+                        </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="gender">Pet Gender</label>
-                                    <select className="form-control" id="gender" required>
-                                        <option value="selected">Select an option</option>
-                                        <option >Female</option>
-                                        <option>Male</option>
-                                    </select>
+                        <div className="form-group">
+                            <label htmlFor="age">Pet Age  (in years old)</label>
+                            <select className="form-control" id="age" required>
+                                <option value="selected">Select an option</option>
+                                <option> under 1</option>
+                                <option> 1</option>
+                                <option> 2</option>
+                                <option> 3</option>
+                                <option> 4</option>
+                                <option> 5</option>
+                                <option> 6</option>
+                                <option> 7</option>
+                                <option> 8</option>
+                                <option> 9</option>
+                                <option> 10</option>
+                                <option> 11</option>
+                                <option> 12</option>
+                                <option> 13</option>
+                                <option> 14</option>
+                                <option> 15</option>
+                                <option> 16</option>
+                                <option> 17</option>
+                                <option> 18</option>
+                                <option> 19</option>
+                                <option> 20</option>
+                                <option> 20 years old and up</option>
+                            </select>
+                        </div>
 
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="type">Pet Kind</label>
-                                    <select className="form-control" id="type" required
-                                        // defaultValue={this.state.selectValue}
-                                        onChange={
-                                            (event) => {
-                                                let selectedValue = event.target.value;
-                                                setCurrentAddedPetKind(selectedValue);
-                                            }
-                                        }>
-                                        <option value="selected">Select an option</option>
-                                        <option>Cat</option>
-                                        <option>Dog</option>
-                                        <option>Rabbit</option>
-                                        <option>Bird</option>
-                                        <option>Reptile</option>
-
-                                    </select>
-                                </div>
-
-
-
-                                <PetBreeds breeds={props.breeds} CurrentAddedPetKind={CurrentAddedPetKind} />
-
-
-
-                                <div className="form-group">
-                                    <label htmlFor="health">Pet Health</label>
-                                    <select className="form-control" id="health" required>
-                                        <option value="selected">Select an option</option>
-                                        <option>Special Needs</option>
-                                        <option>Healthy</option>
-                                    </select>
-                                </div>
-
-                                <br />
-
-                                <label htmlFor="imagePath">Upload Pet Pictures</label>
-                                <br />
-                                <input type="file" className="form-control-file" id="imagePath" required="required" />
-
-
-                                {/* <Link to="/addnewpet/success" className="btn btn-lg text-uppercase btn-light"> */}
-                                <div className="btn btn-lg text-uppercase" >
-                                    <label htmlFor="formSubmitButton">Submit the form here </label>
-                                    <input type="submit" value="submit" class="formSubmitButton" />
-                                </div>
-                                {/* </Link> */}
-
-                            </form>
+                        <div className="form-group">
+                            <label htmlFor="gender">Pet Gender</label>
+                            <select className="form-control" id="gender" required>
+                            <option value="selected">Select an option</option>
+                                <option >Female</option>
+                                <option>Male</option>
+                            </select>
 
                         </div>
 
-                    </main>
+                        <div className="form-group">
+                            <label htmlFor="type">Pet Kind</label>
+                            <select className="form-control" id="type" required
+                                // defaultValue={this.state.selectValue}
+                                onChange={        
+                                    (event) => {
+                                        let selectedValue = event.target.value;
+                                        setCurrentAddedPetKind(selectedValue);
+                                  }
+                                }>
+                            <option value="selected">Select an option</option>
+                                <option>Cat</option>
+                                <option>Dog</option>
+                                <option>Rabbit</option>
+                                <option>Bird</option>
+                                <option>Reptile</option>
+
+                            </select>
+                        </div>
+
+
+
+                        <PetBreeds breeds={props.breeds} CurrentAddedPetKind={CurrentAddedPetKind} />
+
+
+
+                        <div className="form-group">
+                            <label htmlFor="health">Pet Health</label>
+                            <select className="form-control" id="health" required>
+                            <option value="selected">Select an option</option>
+                                <option>Special Needs</option>
+                                <option>Healthy</option>
+                            </select>
+                        </div>
+
+                        <br />
+
+                        <label htmlFor="imagePath">Upload Pet Pictures</label>
+                        <br />
+                        <input type="file" className="form-control-file" id="imagePath" required="required"/>
+                        
+                            <div className="btn btn-lg text-uppercase" >
+                            <label htmlFor="formSubmitButton">Submit the form here </label>
+                                <input type="submit" value="submit" class="formSubmitButton"/>
+                            </div>
+
+
+                    </form>                    
+
                 </div>
-                : <Redirect to="/addnewpet/success" formData={currentData} />
+
+            </main>
+            </div>
+            : <Redirect to="/addnewpet/success" formData={currentData}/>
             }
         </div>
     );
 }
 
 
-export function PetBreeds(props) {
+    export function PetBreeds(props) {
     if (props.CurrentAddedPetKind) {
         let options = [];
         props.breeds[props.CurrentAddedPetKind].forEach((option) => {
@@ -206,24 +193,24 @@ export function PetBreeds(props) {
             <div className="form-group">
                 <label htmlFor="exampleFormControlSelect1">Pet Breed</label>
                 <select className="form-control" id="breed" required>
-                    <option value="selected">Select an option</option>
+                <option value="selected">Select an option</option>
 
                     {options}
                 </select>
             </div>
         );
-    } else {
+    } else{
 
-        return (
-            <div className="form-group">
-                <label htmlFor="exampleFormControlSelect1">Pet Breed</label>
-                <select className="form-control" id="pet-breed">
-                    <option>{props.breeds.undefined}</option>
-                </select>
-            </div>
+    return (
+        <div className="form-group">
+            <label htmlFor="exampleFormControlSelect1">Pet Breed</label>
+            <select className="form-control" id="pet-breed">
+                <option>{props.breeds.undefined}</option>
+            </select>
+        </div>
 
-        );
-    }
+    );
+}
 }
 
 
